2a61e67e066fa484c9b53bfa471ec17f24ef6ad9
Arin Sharma <arinsharma.global@gmail.com>
2025-10-04 13:26:43 +0530

    added-image-upload-feature

--- routes/listing.route.js
+++ routes/listing.route.js
@@ -1,105 +1,52 @@
 const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
-const Listing = require("../models/listing.model.js");
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
 const {
   validateListing,
   isLoggedInUser,
   isOwner,
 } = require("../middleware/middleware.js");
+const {
+  listings,
+  newListing,
+  showListings,
+  createListing,
+  editListing,
+  updateListing,
+  deleteListing,
+} = require("../controllers/listing.controller.js");
 
-//Index Route
-router.get(
-  "/",
-  wrapAsync(async (req, res) => {
-    const allListings = await Listing.find({});
-    res.render("listings/index.ejs", { allListings });
-  })
-);
+//router.route (to combine common path for home route)
+router
+  .route("/")
+  .get(wrapAsync(listings))
+  .post(
+    isLoggedInUser,
+    validateListing,
+    upload.single("listing[image]"),
+    wrapAsync(createListing)
+  );
 
 //New route
-router.get("/new", isLoggedInUser, (req, res) => {
-  res.render("listings/new.ejs");
-});
-
-//Show route
-router.get(
-  "/:id",
-  wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    const listing = await Listing.findById(id)
-      .populate({
-        path: "reviews",
-        populate: {
-          path: "author",
-        },
-      })
-      .populate("owner");
-    if (!listing) {
-      req.flash("error", "The Requested Listing Doesn't Exist Anymore!");
-      return res.redirect("/listings");
-    }
-    console.log(listing);
-    res.render("listings/show.ejs", { listing });
-  })
-);
-
-//Create route
-router.post(
-  "/",
-  isLoggedInUser,
-  validateListing,
-  wrapAsync(async (req, res, next) => {
-    const newListing = new Listing(req.body.listing);
-    newListing.owner = req.user._id;
-    console.log(req.user);
-    await newListing.save();
-    req.flash("success", "New Listing Created!");
-    res.redirect("/listings");
-  })
-);
+router.get("/new", isLoggedInUser, newListing);
 
 //Edit route
-router.get(
-  "/:id/edit",
-  isLoggedInUser,
-  isOwner,
-  wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    const listing = await Listing.findById(id);
-    if (!listing) {
-      req.flash("error", "The Requested Listing Doesn't Exist Anymore!");
-      return res.redirect("/listings");
-    }
-    res.render("listings/edit.ejs", { listing });
-  })
-);
+router.get("/:id/edit", isLoggedInUser, isOwner, wrapAsync(editListing));
 
-//Update route
-router.put(
-  "/:id",
-  isLoggedInUser,
-  isOwner,
-  validateListing,
-  wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-    req.flash("success", "Listing Updated Successfully!");
-    res.redirect(`/listings/${id}`);
-  })
-);
-
-//Delete Route
-router.delete(
-  "/:id",
-  isLoggedInUser,
-  isOwner,
-  wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    let deletedListing = await Listing.findByIdAndDelete(id);
-    req.flash("success", "Listing Deleted Successfully!");
-    res.redirect("/listings");
-  })
-);
+//router.route (to combine common path for listings route)
+router
+  .route("/:id")
+  .get(wrapAsync(showListings))
+  .put(
+    isLoggedInUser,
+    isOwner,
+    upload.single("listing[image]"),
+    validateListing,
+    wrapAsync(updateListing)
+  )
+  .delete(isLoggedInUser, isOwner, wrapAsync(deleteListing));
 
 module.exports = router;
